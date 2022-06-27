01d7c2873dd7267a68d749862b3986bbc8319ec7
Tony Le <letony@icloud.com>
2022-06-27 20:05:45 +1000

    Add scriptable variables

---
+++ Assets/Scripts/Variables.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace LeMinhHuy.Variables
+{
+	[CreateAssetMenu]
+	public class FloatVar : ScriptableObject
+	{
+		public float value;
+	}
+
+	[CreateAssetMenu]
+	public class IntVar : ScriptableObject
+	{
+		public int value;
+	}
+}
