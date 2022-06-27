c05f026f74c6dd4c147f9bf359807a220869ff8f
Tony Le <letony@icloud.com>
2022-06-27 13:20:17 +1000

    Add basic main menu

---
+++ Assets/Scripts/LogToConsole.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace LeMinhHuy
+{
+	public class LogToConsole : MonoBehaviour
+	{
+		public string message = "Message to log to the console";
+
+		public void Log()
+		{
+			Debug.Log(message);
+		}
+
+		public void LogCustomMessasge(string customMessage = "Custom message!")
+		{
+			Debug.Log(customMessage);
+		}
+	}
+}
