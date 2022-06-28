b2963ad330bb293c21ba66b78db256d8d5a07486
Tony Le <letony@icloud.com>
2022-06-28 13:29:59 +1000

    Fence can despawn opponent units

--- Assets/Scripts/Fence.cs
+++ Assets/Scripts/Fence.cs
@@ -2,10 +2,38 @@
 
 namespace LeMinhHuy
 {
+	[RequireComponent(typeof(BoxCollider))]
 	public class Fence : MonoBehaviour
 	{
-		public void SetColor(Color col)
-		=> GetComponent<Renderer>().material.color = col;
+		private Team team;
+		private BoxCollider col;
 
+		void Awake() => col = GetComponent<BoxCollider>();
+
+		void Start()
+		{
+			col.isTrigger = true;
+		}
+
+		public void SetTeam(Team team)
+		{
+			this.team = team;
+			SetColor(team.color);
+		}
+
+		void OnTriggerEnter(Collider other)
+		{
+			var hit = other.GetComponent<Unit>();
+			if (hit is object)
+			{
+				//Unit has hit the fence, despawn if it's not on our team
+				if (!hit.team.Equals(this.team))
+				{
+					hit.Despawn();
+				}
+			}
+		}
+
+		public void SetColor(Color col) => GetComponent<Renderer>().material.color = col;
 	}
 }
