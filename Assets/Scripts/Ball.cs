c5f115b6a67eed22368cfba5945ced5af49dfea8
Tony Le <letony@icloud.com>
2022-06-30 21:26:57 +1000

    Remove agent from Ball

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -13,40 +13,21 @@ namespace LeMinhHuy
 
 		Collider col;
 		Rigidbody rb;
-		// NavMeshAgent agent;
-		//agent required to keep ball on playing field?
-		//And to maybe use the system to pass between players
-		//Go direct, minimal radius, actively seek out receiver
 
 		void Awake()
 		{
 			col = GetComponent<Collider>();
 			rb = GetComponent<Rigidbody>();
-			// agent = GetComponent<NavMeshAgent>();
 		}
 		void Start()
 		{
 			SetActivatePhysics(true);
 		}
 
-		// public void OnEnable()
-		// {
-		// 	//Delay agent every time the ball get's cycled
-		// 	StartCoroutine(Delay());
-		// }
-
-		// IEnumerator Delay()
-		// {
-		// 	agent.enabled = false;
-		// 	yield return new WaitForSeconds(startDelay);
-		// 	agent.enabled = true;
-		// }
-
 		public void SetActivatePhysics(bool active)
 		{
 			col.enabled = active;
 			rb.isKinematic = !active;
-			// agent.enabled = active;
 		}
 
 		/// <summary>
@@ -55,6 +36,8 @@ namespace LeMinhHuy
 		/// <param name="receiver">Receiving unit on the same team</param>
 		public void Pass(Unit receiver)
 		{
+			print("Passing ball to " + receiver.name);
+
 			//Calculate time required to move at the desired speed based on distance
 			//Time = Distance / Speed
 			//Optimization note: This only happens once and not every frame so it's ok
